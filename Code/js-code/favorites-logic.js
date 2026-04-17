f569293a4f6c494f4c2adac1e08c5c857f31db47
LFisher44 <lf44business@gmail.com>
2026-04-16 20:35:43 -0400

    More progress on Favorites
    Favorites can check if the entry you gave it is legitimate now, buttons work, ready to call api. I will implement that next.

--- Code/js-code/favorites-logic.js
+++ Code/js-code/favorites-logic.js
@@ -1,7 +1,7 @@
 //Test section
 console.log("JS loaded");
 
-// Element selection
+// Element / Loading selection
 const addFavBtn = document.querySelector("#showSearchBtn");
 const cancelSearchBtn = document.querySelector("#cancelSearchBtn");
 const favoritesSection = document.querySelector("#favoritesSection");
@@ -9,29 +9,61 @@ const searchSection = document.querySelector("#searchSection");
 const searchBtn = document.querySelector("#searchBtn");
 const searchInput = document.querySelector("#searchInput");
 
+
+async function loadPokemonData() {
+    const response = await fetch("../data/pokemon_dex_name.json");
+    pokemonData = await response.json();
+}
+
+let pokeData = [];
+loadPokemonData();
+
+
 // Event listeners
 addFavBtn.addEventListener("click", function () {
-  favoritesSection.hidden = true;
-  searchSection.hidden = false;
+    favoritesSection.hidden = true;
+    searchSection.hidden = false;
+    favoritesheader.hidden = true;
+
 });
 
 cancelSearchBtn.addEventListener("click", function () {
-  searchSection.hidden = true;
-  favoritesSection.hidden = false;
+    searchSection.hidden = true;
+    favoritesSection.hidden = false;
+    favoritesheader.hidden = false;
 });
 
 searchBtn.addEventListener("click", handleSearch);
 
 //Search Functions
-function handleSearch() {
+/*function handleSearch() {
   console.log("The search button was clicked");
   const userSearch = searchInput.value;
   console.log(userSearch);
    searchSection.hidden = true;
+}*/
+
+function handleSearch() {
+    const userInput = searchInput.value.trim().toLowerCase();
+
+    const result = pokemonData.find(function (temp) {
+        return (
+            temp.name.toLowerCase() === userInput ||
+            temp.id === Number(userInput)
+        );
+    });
+
+    if (result) {
+        console.log("Found:", result);
+    } else {
+        console.log("Not found");
+    }
 }
+
+
 
 //Rendering Functions
 function loadFavorites() {
-  const savedFavorites = localStorage.getItem("favorites");
-  return savedFavorites ? JSON.parse(savedFavorites) : [];
+    const savedFavorites = localStorage.getItem("favorites");
+    return savedFavorites ? JSON.parse(savedFavorites) : [];
 }
