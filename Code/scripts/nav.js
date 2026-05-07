9a1da1a0c437591578ce99966c7dff1f238d32bb
jryingling <yinglingjry@gmail.com>
2026-05-07 00:12:02 -0400

    validate search dex number

--- Code/scripts/nav.js
+++ Code/scripts/nav.js
@@ -4,13 +4,16 @@ function goToPokemon(dex) {
 
 function validateSearch() {
   const pokeSearch = document.getElementById("pokemon-search").value.trim();
+  const dexNum = parseInt(pokeSearch);
   const match = pokemon.find(
-    (p) => p.name.toLowerCase() === pokeSearch.toLowerCase(),
+    (p) =>
+      p.name.toLowerCase() === pokeSearch.toLowerCase() ||
+      (!isNaN(dexNum) && p.dex === dexNum),
   );
 
   if (!match) {
     console.log("Pokemon does not exist");
-    return Promise.reject("Invalid pokemon name.");
+    return Promise.reject("Invalid pokemon name or dex number.");
   } else {
     return Promise.resolve([match.dex]);
   }
